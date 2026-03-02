38a15509a4e12a328f40bc9c7b01e6f1a3cd8726
cinthyaertic08-hash <cinthya.ertic08@gmail.com>
2026-03-02 09:46:39 -0600

    Add files via upload

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,15 @@
+//obtener el boton por su ID que se puso
+const boton1=document.getElementById('lily');
+
+//Agregar un evento al boton
+boton1.addEventListener('click', function(){
+	alert("ESTATUA DE LA LIBERTAD");
+});
+function toggleMobileMenu() {
+    const menu = document.getElementById("mobileMenu");
+    if (menu.style.width === "100%") {
+        menu.style.width = "0";
+    } else {
+        menu.style.width = "100%";
+    }
+}
